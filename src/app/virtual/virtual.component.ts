99ac40d60e9f68ae16c0635d9da23de7ae9a3b4e
JohnErikcObando <Jeobando11@hotmail.com>
2021-04-05 20:47:39 -0500

    implementacion de paises con Drag

--- src/app/virtual/virtual.component.ts
+++ src/app/virtual/virtual.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-virtual',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VirtualComponent implements OnInit {
 
+  @ViewChild(CdkVirtualScrollViewport) viewport: CdkVirtualScrollViewport;
+
+  personas = Array(500).fill(0);
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.personas);
+    
+  }
+
+  irFinal(){
+      this.viewport.scrollToIndex(this.personas.length);
+  }
+
+  irInicio(){
+    this.viewport.scrollToIndex(0);
+  }
+
+  irMitad(){
+    this.viewport.scrollToIndex(this.personas.length / 2);
   }
 
 }
